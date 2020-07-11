a51c116301c182265c3079d3d70ed78c5d332f64
Jefanya Efandchris <63415527+jefanyae@users.noreply.github.com>
2020-07-11 10:49:19 +0700

    Update kerja.py

--- userbot/modules/kerja.py
+++ userbot/modules/kerja.py
@@ -71,7 +71,7 @@ async def typewriter(typew):
 async def typewriter(typew):
 	message = typew.pattern_match.group(0)
 	sleep(0)
-	await typew.edit("`Tampilan cek di : https://jeenoreal.com/`")
+	await typew.edit("`Tampilan cek di :` https://jeenoreal.com/")
 	
 @register(outgoing=True, pattern='^.proses(?: |$)(.*)')
 async def typewriter(typew):
